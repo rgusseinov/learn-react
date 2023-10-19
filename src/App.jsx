17d430b89bef4d6228a57f8851a58bdf7afe828f
ruslan-at-615135307338 <r.gusseinov@mail.ru>
2023-10-19 12:59:14 +0600

    add feature #1

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,7 @@ const App =() => {
       <br />
       <p>This is staging project</p>
       <br />
-      <p>1. Upload file</p>
-      <br />
-      <p>2. Upload multiple files. (Fixed uploading .png files in CMS)</p>
-      <p>3. Added Approval Route </p>
-      <p> Feature 2 (add upload file in VEF module) </p>
-      <p> Feature 2.1 (Add PDF generation) / prioriry: High </p>
-      <p> Feature 2.2. Remove .env file </p>
+      <p>Added feature #1</p>
     </div>
   )
 }
